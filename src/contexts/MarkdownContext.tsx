3f953e244038ffdb234f9aaea74961baa219a8f4
kiranmarshall <kiran.marshall@gmail.com>
2022-04-24 13:40:13 +0100

    feat: add new functionality for handling markdown

--- src/contexts/MarkdownContext.tsx
+++ src/contexts/MarkdownContext.tsx
@@ -1,16 +1,38 @@
-import { createContext, FC, useContext, useState } from 'react';
+import { createContext, FC, useContext, useEffect, useMemo, useState } from 'react';
+import { ObjectRecord } from '@/hooks';
 
 interface MarkdownContext {
    markdown: Record<string, any>;
    setMarkdown: React.Dispatch<React.SetStateAction<Record<string, any>>>;
+   setDefaultMarkdown: React.Dispatch<React.SetStateAction<Record<string, any>>>;
+   addHeaderToMarkdown: (headers: ObjectRecord[]) => void;
+   renderedMarkdown: string;
 }
 
 const Context = createContext({} as MarkdownContext);
 
 export const MarkdownProvider: FC = (props) => {
-   const [markdown, setMarkdown] = useState<Record<string, any>>({});
+   const [markdown, setMarkdown] = useState<ObjectRecord>({});
+   const [defaultMarkdown, setDefaultMarkdown] = useState<ObjectRecord>({});
 
-   return <Context.Provider value={{ markdown, setMarkdown }} {...props} />;
+   useEffect(() => {
+      setMarkdown(defaultMarkdown);
+   }, [defaultMarkdown]);
+
+   const addHeaderToMarkdown = (headers: ObjectRecord[]) => {
+      setMarkdown(() => Object.assign({}, defaultMarkdown, ...headers));
+   };
+
+   const renderedMarkdown = useMemo(() => renderMarkdown(markdown), [markdown]);
+
+   const ctx = { markdown, setMarkdown, setDefaultMarkdown, addHeaderToMarkdown, renderedMarkdown };
+
+   return <Context.Provider value={ctx} {...props} />;
 };
 
 export const useMarkdown = () => useContext(Context);
+
+export const renderMarkdown = (markdown: ObjectRecord) => {
+   const arr = Object.entries(markdown);
+   return arr.map((item) => `* *${item[0]}*: \`\`\`${item[1]}\`\`\`  `).join('');
+};
