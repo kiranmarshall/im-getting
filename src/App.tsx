cbc717404445c38e514286fa16aacd09ea8a5c15
Kiran Marshall <kmarshall@axiamatic.com>
2022-05-01 20:29:49 +0100

    fix: update counter styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import './index.css';
 const App = () => {
    const { errors } = useErrors();
 
+   const bigRedClass = 'place-self-center text-7xl font-extrabold tracking-tighter text-red-500';
+
    return (
       <div className="flex flex-col space-y-4 p-10">
-         <div className="flex items-center flex-wrap lg:w-[1000px]">
-            <h1 className=" place-self-center text-7xl font-extrabold tracking-tighter text-red-500">I'm getting...</h1>
+         <div className="flex items-center flex-wrap lg:w-[1400px]">
+            <h1 className={bigRedClass}>I'm getting...</h1>
             <UploadPanel />
          </div>
 
-         {!!errors.length && <span>Total Errors: {errors.length}</span>}
+         {!!errors.length && <span className={`${bigRedClass} text-gray-600 text-4xl`}>Total: {errors.length}</span>}
 
          {errors.map((error) => (
             <MarkdownProvider key={error.time}>
